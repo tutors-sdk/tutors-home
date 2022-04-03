1bd3f2f4064ef907d83318e41c431c02b5093c7d
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-04-03 20:43:07 +0100

    Revert "Merge branch 'main' of https://github.com/tutors-sdk/tutors-home"
    This reverts commit ccfd84e6508e3081e08ac882924c60d177905165, reversing
    changes made to c7b6995bd3c4f2b9dbc099ccd4240cfb09caab20.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,3 @@
-import svelte from '@astrojs/svelte';
-
 export default {
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
@@ -14,7 +12,7 @@ export default {
     // port: 3000,             // The port to run the dev server on.
     tailwindConfig: './tailwind.config.js',
   },
-  integrations: [
-    svelte(),
+  renderers: [
+    "@astrojs/renderer-svelte"
   ],
 };
